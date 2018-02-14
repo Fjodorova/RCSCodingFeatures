08f80fa8282d1427b80b662abd50c3bcc81f2b25
Fjodorova <36454616+Fjodorova@users.noreply.github.com>
2018-02-14 18:01:22 +0200

    Pienoju HelloWorldApplikāciju

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            SayHelloToUser();
+        }
+
+        static void SayHelloToUser()
+        {
+            Console.WriteLine("Please enter your name and age:");
+            string usersName;
+            usersName = Console.ReadLine();
+            string usersAge;
+            usersAge = Console.ReadLine();
+            Console.WriteLine("Labdien," + usersName + "Tavs vecums ir:" + usersAge + "!");
+            Console.ReadLine();
+            
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
